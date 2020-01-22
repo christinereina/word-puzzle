971b87a975e398fa4f4e975f9e0a85b24d83bc09
Christine Augustine and jeremy kale padot <christine@>
2020-01-22 13:45:10 -0800

    add working loop for word puzzle

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+$('form.ferm').submit(function(event) {
+  var string = $('input#sentence').val();
+  var letters = string.split('');
+  console.log(letters);
+
+  for (var index =0; index < letters.length; index +=1) {
+      if (letters[index] === 'a' || letters [index] === 'e' || letters [index] === 'i' || letters [index] === 'o' || letters [index] === 'u' ) {
+          letters.splice(index,1, '-')
+
+      }
+  }
+
+    $('p').text(letters.join(' '));
+    $('form.ferm').hide();
+
+    event.preventDefault();
+
+    });
+});
